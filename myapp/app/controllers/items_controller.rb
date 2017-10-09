5842f6eb4c9dc806e34f2107e44f42e09bcf6466
patrick <patrick.labes@gmail.com>
2017-10-09 01:23:06 +0000

    Has the original github source as well

--- myapp/app/controllers/items_controller.rb
+++ myapp/app/controllers/items_controller.rb
@@ -1,8 +1,25 @@
 class ItemsController < ApplicationController
+    def index
+        @items = Item.all
+    end
+    
+    def show
+        @item = Item.find(params[:id])
+    end
+    
     def new
     end
     
     def create
-        render plain: params[:item].inspect
+        @item = Item.new(item_params)
+        
+        @item.save
+        redirect_to @item
     end
+    
+    private
+    def item_params
+        params.require(:item).permit(:title, :notes)
+    end
+    
 end
